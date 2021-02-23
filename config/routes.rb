30145f75aef8022b4c4f86fb4ea87fec53700493
RINA <74470264+RINA-11@users.noreply.github.com>
2021-02-23 16:45:35 +0000

    commit-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get "home/top"
+  
+  resources :users#, only: [:new, :create, :show]
+  
+  get "home/top" => "home#top"
   root "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
