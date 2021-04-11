0823df176c0a068489c8da4807e0e262ef3faa13
RINA <74470264+RINA-11@users.noreply.github.com>
2021-04-11 14:20:31 +0000

    commit-39

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -9,13 +9,13 @@ class ClipsController < ApplicationController
   
   def create
     @clip = Clip.create(user_id: current_user.id, post_id: params[:post_id])
-    redirect_to post_path(id: params[:post_id])
+    redirect_to post_path(id: params[:post_id], anchor: "clip")
   end
   
   def destroy
     @clip = Clip.find_by(user_id: current_user.id, post_id: params[:post_id])
     @clip.destroy
-    redirect_to post_path(id: params[:post_id])
+    redirect_to post_path(id: params[:post_id], anchor: "unclip")
   end
   
 end
